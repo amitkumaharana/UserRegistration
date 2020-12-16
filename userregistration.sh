f3e296cfdabb48bb3296fde961d2c753ebead189
amitkumaharana <ak657maharana@gmail.com>
2020-12-16 11:58:01 +0530

    To check for valid email id

--- userregistration.sh
+++ userregistration.sh
@@ -5,16 +5,28 @@ read first_name
 first_name_pat='^[A-Z]{1}[a-z]{2,}$'
 if [[ $first_name =~ $first_name_pat ]]
 then
-	echo Enter your last name
-	read last_name
-	last_name_pat='^[A-Z]{1}[a-z]{2,}$'
-	if [[ $last_name =~ $last_name_pat ]]
-	then
-	        echo Your Name is $first_name $last_name
-	else
-	        echo "Invalid Input for Last name"
-	fi
+	echo Your First Name is $first_name
 
 else
 	echo "Invalid Input for First name"
 fi
+echo Enter your last name
+read last_name
+last_name_pat='^[A-Z]{1}[a-z]{2,}$'
+if [[ $last_name =~ $last_name_pat ]]
+then
+        echo Your Last Name is $last_name
+	echo Your name is $first_name $last_name
+else
+        echo "Invalid Input for Last name"
+fi
+echo Enter your Email address
+read email
+email_pat='^[0-9A-Za-z]+([._-][0-9A-Za-z]+)*@[0-9A-Za-z]{2,4}[.][0-9A-Za-z]{2,4}([.][0-9A-Za-z]{2})*$'
+if [[ $email =~ $email_pat ]]
+then
+	echo You have a valid email id
+else
+	echo Invalid email id
+fi
+
