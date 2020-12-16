3464b6bd33af2107b90500fcf1394bb3e913c02e
amitkumaharana <ak657maharana@gmail.com>
2020-12-16 07:54:48 +0530

    Entered valid Last name

--- userregistration.sh
+++ userregistration.sh
@@ -5,8 +5,16 @@ read first_name
 first_name_pat='^[A-Z]{1}[a-z]{2,}$'
 if [[ $first_name =~ $first_name_pat ]]
 then
-	echo Your First name is $first_name
-else
-	echo Invalid Input
-fi
+	echo Enter your last name
+	read last_name
+	last_name_pat='^[A-Z]{1}[a-z]{2,}$'
+	if [[ $last_name =~ $last_name_pat ]]
+	then
+	        echo Your Name is $first_name $last_name
+	else
+	        echo "Invalid Input for Last name"
+	fi
 
+else
+	echo "Invalid Input for First name"
+fi
