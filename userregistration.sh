0847b5bdb40ae94b79bfab3999c0dea5bbd0ec7a
amitkumaharana <ak657maharana@gmail.com>
2020-12-16 12:18:07 +0530

    To check valid mobile number

--- userregistration.sh
+++ userregistration.sh
@@ -29,4 +29,12 @@ then
 else
 	echo Invalid email id
 fi
-
+echo Enter a valid countrycode follwed by a space follwed by a 10 digit mobile number
+read mobile
+mobile_pat='^[9]{1}[1]{1}[[:space:]][0-9]{10}$'
+if [[ $mobile =~ $mobile_pat ]]
+then
+	echo Valid number
+else
+	echo Invalid number
+fi
