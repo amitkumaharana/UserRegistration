9e78938c59fa9aac836183aa0b19a6f5b274b166
amitkumaharana <ak657maharana@gmail.com>
2020-12-16 07:41:21 +0530

    Entered First name

--- userregistration.sh
+++ userregistration.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo Welcome to User Registration Program
+echo Enter your first name
+read first_name
+first_name_pat='^[A-Z]{1}[a-z]{2,}$'
+if [[ $first_name =~ $first_name_pat ]]
+then
+	echo Your First name is $first_name
+else
+	echo Invalid Input
+fi
+
